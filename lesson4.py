180d18327ac30b0bd5c4b15fe3ed1ffee3b08297
JosephRidge <josephridge18@gmail.com>
2026-06-15 20:33:20 +0300

    update

--- lesson4.py
+++ lesson4.py
@@ -3,10 +3,12 @@ Control Flows:
     - conditional statements:
         - if...else
         - if...elif...else
-        - ternary operarot
-        - match scenario
+        - ternary operator
+        - match...case
 
-    - loops
+    - loops:
+        - for...loop
+        - while
 """
 
 output = "" # globa variable 
@@ -42,7 +44,53 @@ match (color):
     case _: # depicts default
         output = "I dot know" 
 
+#  for .. loop
+fruits = ["mangoes","bananas", "pineapples", "kiwi", "watermelon", "oranges", "beetroots"]
 
+for fruit in fruits:
+    print(f"=>>> {fruit.upper()}") 
+
+basket_size = len(fruits)
+
+while basket_size > 0:
+    if basket_size == 0:
+        pass 
+    else: 
+        print("you still have fruits!")
+        fruits.pop()
+        basket_size = len(fruits)
+
+fruits = ["mangoes","bananas", "pineapples", "kiwi", "watermelon", "oranges", "beetroots"]
+basket_size = len(fruits)
+
+# ============ Order of exeution implication example ============
+# while basket_size > 0:
+#     print("you still have fruits!")
+#     if basket_size == 0:
+#         pass 
+#     else: 
+#         fruits.pop()
+#         basket_size = len(fruits)
+
+# order of execution difference        
+# x = 0 
+# while x <=10: 
+#     print(x)
+#     x+=1 # increment by 1
+# print("------------------")
+# x=0
+# while x <=10:
+#     x+=1 # increment by 1
+#     print(x) 
+# ============ Order of exeution implication example ============
+
+
+#  if using numbers as the ranges
+# range(10): moves from 0 to 9
+# range(1, 10): moves from 1 to 9
+# range (2, 18, 2): moves from 2 to 17 with a step of 2
+for i in range(2, 18, 2):
+    print(i)
 
 print("=============================================")
 print(output)
